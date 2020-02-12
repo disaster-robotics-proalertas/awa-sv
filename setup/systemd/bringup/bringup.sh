71850d389e9366fc41f87be6a294eab158216949
rgmaidana <renanmaidana@gmail.com>
2020-02-12 16:06:02 -0300

    Fixed small bug with if on bringup script

--- setup/systemd/bringup/bringup.sh
+++ setup/systemd/bringup/bringup.sh
@@ -5,6 +5,7 @@ source /opt/ros/kinetic/setup.bash
 # If processor is Rasp Zero, don't source catkin workspace
 PROC_REV=$(cat /proc/cpuinfo | grep Revision | awk '{print $3}')
 if [ "$PROC_REV" == "9000c1" ]; then
+    continue
 else
     source $CATKIN_PATH/devel/setup.bash
 fi
