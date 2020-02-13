7d44abb750e16eb8224074248e64d894abfbc8bd
rgmaidana <renanmaidana@gmail.com>
2020-02-13 16:28:42 -0300

    Check and update sonar IP before launching sonar node

--- setup/systemd/sonar/startup_launch_ros_sonar.sh
+++ setup/systemd/sonar/startup_launch_ros_sonar.sh
@@ -9,5 +9,8 @@ else
     source $CATKIN_PATH/devel/setup.bash
 fi
 
-sed -i "/sonar/ s/.*/$(/usr/bin/nmap -sP 192.168.1.0/24 >/dev/null && arp -an | grep 98:7b:f3:1b:d4:f9 | awk '{print $2}' | sed 's/[()]//g')\tsonar/g" /etc/hosts
-roslaunch ros_nmea_depth lowrance.launch
+if [ ! -z "$(/usr/bin/nmap -sP 192.168.1.0/24 >/dev/null && arp -an | grep 98:7b:f3:1b:d4:f9 | awk '{print $2}' | sed 's/[()]//g')" ]; then
+        roslaunch ros_nmea_depth lowrance.launch address:="$(/usr/bin/nmap -sP 192.168.1.0/24 >/dev/null && arp -an | grep 98:7b:f3:1b:d4:f9 | awk '{print $2}' | sed 's/[()]//g')"
+else
+        exit 1
+fi
