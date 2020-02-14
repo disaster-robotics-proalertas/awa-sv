446c559ced2c813a5b5b073664c623dfdccf6e96
rgmaidana <renanmaidana@gmail.com>
2020-02-14 19:11:01 -0300

    Added files for install and configuration on jetson tk1 (with upstart)

---
+++ setup/upstart/bringup/bringup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+source /opt/ros/kinetic/setup.bash
+
+# If processor is Rasp Zero, don't source catkin workspace
+PROC_REV=$(cat /proc/cpuinfo | grep Revision | awk '{print $3}')
+if [ "$PROC_REV" == "9000c1" ]; then
+    :
+else
+    source $CATKIN_PATH/devel/setup.bash
+fi
+
+roslaunch asv_bringup minimal.launch
